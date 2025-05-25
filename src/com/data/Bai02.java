4afc0cc68e7bdac939fdd54ff112d355e3a84d6b
Tran Duy Khang <trank7866@gmail.com>
2025-05-25 18:34:54 +0700

    BTVN Buoi03

--- src/com/data/Bai02.java
+++ src/com/data/Bai02.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Bai02 {
     public static void main(String[] args) {
-        int S,n;
-        S = 0;
+        int s,n;
+        s = 0;
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhập số: ");
         do {
             n = sc.nextInt();
-            S+=n;
+            s+=n;
         } while (n!=0);
 
-        System.out.println("Tổng các số đã nhập là: " + S);
+        System.out.println("Tổng các số đã nhập là: " + s);
     }
 }
